0126213ca6a66c464b5306f5141a1865b048adcc
moreka <mr.karegar@gmail.com>
2024-03-02 01:42:53 +0330

    prevent duplicate time generation for a day

--- src/main/java/com/blubank/doctorappointment/application/domain/service/OpenTimeService.java
+++ src/main/java/com/blubank/doctorappointment/application/domain/service/OpenTimeService.java
@@ -1,13 +1,19 @@
 package com.blubank.doctorappointment.application.domain.service;
 
+import com.blubank.doctorappointment.application.domain.model.VisitTime;
 import com.blubank.doctorappointment.application.port.in.OpenTimeCommand;
 import com.blubank.doctorappointment.application.port.in.OpenTimeServiceUseCase;
+import com.blubank.doctorappointment.application.port.out.LoadVisitTimePort;
 import com.blubank.doctorappointment.application.port.out.UpdateVisitTimePort;
 import com.blubank.doctorappointment.common.UseCase;
 
 
+import com.blubank.doctorappointment.common.dto.DateTimeDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 
 @RequiredArgsConstructor
@@ -17,10 +23,17 @@ public class OpenTimeService implements OpenTimeServiceUseCase {
 
     private final TimeGenerator timeGenerator;
     private final UpdateVisitTimePort updateVisitTimePort;
+    private LoadVisitTimePort loadVisitTimePort;
 
     @Transactional
     @Override
     public void openTimePeriod(OpenTimeCommand command) {
+        DateTimeDto open = command.open();
+        List<VisitTime> alreadyCreatedTimes = loadVisitTimePort
+                .loadDoctorTimes(LocalDateTime.of(open.getYear(), open.getMonth(), open.getDay(), 0, 0));
+        if (!alreadyCreatedTimes.isEmpty()) {
+            throw new RuntimeException("times already opened for this day!");
+        }
         var visitTimes = timeGenerator.generate(command.open().getDate(), command.end().getDate());
         updateVisitTimePort.addAll(visitTimes);
     }
